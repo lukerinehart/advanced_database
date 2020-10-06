70c956cf477c0862be2bca1e75ce26a72a98f607
lukerinehart <lukerinehart79@gmail.com>
2020-10-05 21:46:39 -0400

    cleaned up/made readable

--- production/bottle_app.py
+++ production/bottle_app.py
@@ -1,85 +1,66 @@
-from bottle import get, post, template, request, redirect
-import sqlite3
 import os
+import sqlite3
+from bottle import get, post, template, request, redirect
 
-# Are we executing at pythonanywhere?
+# at PythonAnywhere?
 ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ
- 
-# Local vs Pythonanywhere
+
 if ON_PYTHONANYWHERE:
+    # on PA
     from bottle import default_app
 else:
+    # on the development environment
     from bottle import run, debug
+
+from storage import get_items, get_item, update_status, create_item, update_item, delete_item
+
 
 @get('/')
 def get_show_list():
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("select * from todo")
-    result = cursor.fetchall()
-    cursor.close()
-    #return str(result)
-    return template("show_list", rows = result)    # creates file, templates are like html
+    result = get_items()
+    return template("show_list", rows=result)
+
 
 @get("/set_status/<id:int>/<value:int>")
-def set_status(id, value):
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("update todo set status=? where id=?", (value,id))
-    connection.commit()
-    cursor.close()
+def get_set_status(id, value):
+    update_status(id, value)
     redirect("/")
 
 
 @get("/new_item")
-def get_new_item(): #returns template for a form to fill out
+def get_new_item():
     return template("new_item")
 
+
 @post("/new_item")
-def post_new_item():    
+def post_new_item():
     new_item = request.forms.get("new_item").strip()
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("insert into todo (task, status) values(?,?)", (new_item, 1))
-    #cursor.lastrowid
-    connection.commit()
-    cursor.close()
-    #return "The new item is  [" + new_item + "] ..."
+    create_item(new_item, 1)
     redirect("/")
-
-
-@get('/delete_item/<id:int>')   # Guarantees it grabs an int
-def get_delete_item(id):
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("DELETE from todo WHERE id=?", (id,) )    #A touple, ie 10 would be 1 and 0 without it
-    connection.commit()
-    cursor.close()
-    redirect("/")
-
 
 @get("/update_item/<id:int>")
 def get_update_item(id):
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("select * from todo where id=?", (id,))
-    result = cursor.fetchall()
-    cursor.close()
-    return template("update_item", row=result[0])
+    result = get_item(id)
+    return template("update_item", row=result)
+
 
 @post("/update_item")
-def post_update_item():    
+def post_update_item():
     id = int(request.forms.get("id").strip())
     updated_item = request.forms.get("updated_item").strip()
-    connection = sqlite3.connect("todo.db")
-    cursor = connection.cursor()
-    cursor.execute("update todo set task=? where id=?", (updated_item, id))
-    connection.commit()
-    cursor.close()
+    update_item(id, updated_item)
+    redirect("/")
+
+
+@get("/delete_item/<id:int>")
+def get_delete_item(id):
+    delete_item(id)
     redirect("/")
 
 if ON_PYTHONANYWHERE:
-     application = default_app()
+    # on PA
+    application = default_app()
 else:
+    # on the development environment
     debug(True)
     run(host='localhost', port=8080)
